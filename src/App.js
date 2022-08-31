511c338672c7fb5649df9488e69f06d17eae9a6e
paul serafim <paul.serafim.3021@gmail.com>
2022-08-31 11:38:09 +1000

    completed sidebar

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import './App.css'
 import Header from './Header'
 import './App.css';
 import Sidebar from './Sidebar'
+import Mail from './Mail'
+import EmailList from './EmailList'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  BrowserRouter
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className='app'>
-      <Header />
-      <Sidebar />
+    <BrowserRouter>
+      <div className='app'>
+        <Header />
+
+        <div className="app__body">
+          <Sidebar />
+
+          <Routes>
+            <Route path="/mail" element={<Mail />} />
+            <Route path="/" element={<EmailList />} />
+          </Routes>
+        </div>
     </div>
+    </BrowserRouter>
+    
   );
 }
 
---
+++ src/EmailList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './EmailList.css'
+
+const EmailList = () => {
+    return (
+        <div className="emailList">
+            <div className="emailList__settings">
+
+            </div>
+        </div>
+    );
+}
+
+export default EmailList;
